c05703a66d44640cdefeb14221fe6f9150d1b829
RyotaNISHIOKA <mlf.r24oka@gmail.com>
2020-06-17 19:23:44 +0900

    providerパターンでモデルを作ってみる

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:nandemoii/main_model.dart';
 import 'package:nandemoii/next_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,116 +12,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-  final String title;
-  String name;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  final items = [//List<String>.generate(10000, (i) => "Item $i");
-    'kboy 1', 'kboy 2', 'kboy 3', '4',
-  ];
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('KBOYのFlutter大学!!!'),
-        actions: <Widget>[
-          Icon(Icons.add),
-          Icon(Icons.share)
-        ],
-      ),
-      body: Container(
-        width: double.infinity,
-        child:ListView(
-          // This next line does the trick.
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-
-            Column(
-              children: <Widget>[
-                Expanded(
-                  child: Image.network(
-                  'https://pbs.twimg.com/profile_images/1269267428476719106/6yYseCYR_400x400.jpg'
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('KBOYのFlutter大学!!!'),
+          ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+            return Center(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    model.kboyText,
+                    style: TextStyle(fontSize: 30),
                   ),
-                ),
-                Text('kboy'),
-              ],
-            ),
-
-
-            Column(
-              children: <Widget>[
-                Expanded(
-                  child: Image.network(
-                      'https://pbs.twimg.com/profile_images/1269267428476719106/6yYseCYR_400x400.jpg'
-                  ),
-                ),
-                Text('kboy'),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Expanded(
-                  child: Image.network(
-                      'https://pbs.twimg.com/profile_images/1269267428476719106/6yYseCYR_400x400.jpg'
-                  ),
-                ),
-                Text('kboy'),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Expanded(
-                  child: Image.network(
-                      'https://pbs.twimg.com/profile_images/1269267428476719106/6yYseCYR_400x400.jpg'
-                  ),
-                ),
-                Text('kboy'),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Expanded(
-                  child: Image.network(
-                      'https://pbs.twimg.com/profile_images/1269267428476719106/6yYseCYR_400x400.jpg'
-                  ),
-                ),
-                Text('kboy'),
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Expanded(
-                  child: Image.network(
-                      'https://pbs.twimg.com/profile_images/1269267428476719106/6yYseCYR_400x400.jpg'
-                  ),
-                ),
-                Text('kboy'),
-              ],
-            ),
-          ],
+                  RaisedButton(
+                    child: Text('ボタン'),
+                    onPressed: () {
+                      //ここでなにか
+                      model.changeKboyText();
+                    },
+                  )
+                ],
+              ),
+            );
+          }),
         ),
-    ),
+      ),
     );
   }
 }
